1162ce250bd42ff7397630280d31310f6ff6b027
uscgithubuser <144652288+uscgithubuser@users.noreply.github.com>
2023-09-13 21:25:10 +0800

    Update stacklinkedlist.h

--- stacklinkedlist.h
+++ stacklinkedlist.h
@@ -1,5 +1,72 @@
 #ifndef MY_LIB
 #define STACKLINKEDLIST_H
 // Function definition goes here
+typedef enum{
+    F, T
+} boolean;
 
+typedef struct node{
+	char data;
+	struct node* link;
+}*LIST;
+
+void initialize(LIST *A);
+void display(LIST A);
+void push(LIST *A, char newElem);
+void pop(LIST *A);
+int top(LIST A);
+boolean isEmpty(LIST A);
+boolean isFull(LIST A);
+
+void initialize(LIST *A){
+	*A = NULL;
+}
+
+void display(LIST A){
+	LIST temp;
+	for(temp = A; temp != NULL; temp = temp->link){
+		printf("[%c]->", temp->data);
+	}
+}
+
+void push(LIST *A, char newElem){
+	LIST newNode;
+	newNode = (LIST)malloc(sizeof(struct node));
+	if(newNode == NULL){
+		printf("Allocation Failed!");
+	}
+	else{
+		if(*A == NULL){
+			newNode->data = newElem;
+			newNode->link = NULL;
+			*A = newNode;
+		}
+		else{
+			newNode->data = newElem;
+			newNode->link = *A;
+			*A = newNode;
+		}
+	}
+
+}
+
+void pop(LIST *A){
+	LIST temp;
+	temp = *A;
+	*A = (*A)->link;
+	free(temp);
+}
+
+int top(LIST A){
+	int topVal;
+	
+}
+
+boolean isEmpty(LIST A){
+	
+}
+
+boolean isFull(LIST A){
+	
+}
 #endif
