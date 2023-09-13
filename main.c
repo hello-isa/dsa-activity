ff5f8511a25602328bb1f5a743c2d7363798f1fd
hello-isa <isabeljoyadriatico@gmail.com>
2023-09-13 18:32:12 +0800

    Created main.c or the driver program

---
+++ main.c
@@ -0,0 +1,9 @@
+// This is the driver program
+#include <stdio.h>
+#include "stackarray.h"      // For array implementation
+#include "stacklinkedlist.h" // For linked list implementation
+
+int main()
+{
+    return 0;
+}
